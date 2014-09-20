26ae9c01e80b2dc2ee2a92e851b7e6c16b872792
Syd Polk <sydpolk@gmail.com>
2014-09-20 00:23:39 -0500

    Add CoreDataDumper.

---
+++ CoreDataDumper/CoreDataDumper/Run.m
@@ -0,0 +1,35 @@
+//
+//  Run.m
+//  CoreDataDumper
+//
+//  Created by Syd Polk on 8/4/14.
+//  Copyright (c) 2014 Bone Jarring Games and Software. All rights reserved.
+//
+
+
+#import "Run.h"
+
+@interface Run ()
+
+@property (strong) NSDate *primitiveDate;
+
+@end
+
+@implementation Run
+
+@dynamic date, primitiveDate, processID;
+
+- (void) setNilValueForKey:(NSString *)key {
+    if ([key isEqualToString:@"processID"]) {
+        self.processID = 0;
+    } else {
+        [super setNilValueForKey:key];
+    }
+}
+
+- (void) awakeFromInsert {
+    [super awakeFromInsert];
+    self.primitiveDate = [NSDate date];
+}
+
+@end
